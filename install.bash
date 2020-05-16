73f997882d59f9110768d780954187c2e91fe48b
jotelfor <jtelford1@gmail.com>
2020-05-16 19:51:43 +0100

    Detect IP address of instance.

--- install.bash
+++ install.bash
@@ -1,12 +1,16 @@
 sudo yum -y update
 
+sudo yum -y install git
+git clone https://github.com/johntelforduk/hello-world.git
+
 yes | sudo amazon-linux-extras install nginx1.12
-sudo cp flask.conf /etc/nginx/conf.d/+
+my_ip=$(curl http://checkip.amazonaws.com)
+sudo cp hello-world/flask.conf /etc/nginx/conf.d/
+sudo sed -i 's/$my_ip/'$my_ip'/g' /etc/nginx/conf.d/flask.conf
+
 sudo systemctl restart nginx
 
 sudo yum -y install python3
-sudo yum -y install git
-git clone https://github.com/johntelforduk/hello-world.git
 sudo pip3 install flask
 
 echo python3 ./hello-world/hello-flask.py
